34ccf9cbe01aebc2fbbbaab62dba283422b7194b
Mayur Ingle <mayur.ingle8@gmail.com>
2017-01-22 15:04:33 +0530

    Update Exception.java

--- com/practice/others/Exception.java
+++ com/practice/others/Exception.java
@@ -1,16 +1,29 @@
 package com.practice.others;
 
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+//Checked and unchecked execption 
 
-public class Exception {
-	public static void main(String a[]) {
+
+public class ExceptionTest {
+	public static void main(String a[]) throws FileNotFoundException {
 		
-		int a1, b,c;
-		a1=2;
-		b=0;
-		System.out.println(a1/b);
+		try{
+			int a1, b,c;
+			a1=8;
+			b=4;
+			int d=0;
+			System.out.println("One "+a1/b);
+		//System.exit(0);
+		System.out.println(a1/d);
+		}catch(Exception e){
+			e.printStackTrace();
+			//System.out.println(e);
+		}finally{
+			System.out.println("In finally ");
+		}
 		
 		
 	}
